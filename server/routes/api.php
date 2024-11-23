a97aee183b1b6c6d4f43cc6089e423ae70222164
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-23 20:48:39 +0200

    Changed routes to api.php

---
+++ server/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\JournalController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix("user")->withoutMiddleware(["csrf"])->group(function(){
+  Route::post("/login", [UserController::class, "login"]);
+  Route::post("/register", [UserController::class, "register"]);
+});
+
+
+Route::prefix("journal")->group(function(){
+  Route::get("/{id}", [JournalController::class, "getJournals"]);
+  Route::post("/{id}", [JournalController::class, "postJournal"]);
+  Route::put("/{id}", [JournalController::class, "editJournal"] );
+  Route::delete("/{id}", [JournalController::class, "removeJournal"]);
+});
+
+Route::prefix("article")->group(function(){
+  Route::get("/", [ArticleController::class, "getArticles"]);
+  Route::get("/{id}", [ArticleController::class, "getArticlesByUser"] );
+  Route::post("/{id}", [ArticleController::class, "postArticle"]);
+});
