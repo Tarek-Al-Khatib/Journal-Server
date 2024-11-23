31927485273df66958e6b09f8a1ce637c597aea5
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-23 20:07:54 +0200

    Added create journal

--- server/app/Http/Controllers/JournalController.php
+++ server/app/Http/Controllers/JournalController.php
@@ -18,4 +18,31 @@ class JournalController extends Controller
 
         return response()->json($journals);
     }
+
+    public function postJournal(Request $request, $id){
+
+        $user = User::find($id);
+        if(!$user){
+            return response()->json(["message" => "Not found"], 404);
+        }
+
+        if($user->role != "admin"){
+            return response()->json(["message" => "You are not authorized to do this"], 401);
+        }
+
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'text' => 'required|string',
+            'posted_by' => 'required|exists:users,id',
+            'restrictedAge' => 'nullable|integer|min:0',
+        ]);
+
+
+        $journal = Journal::create($validated);
+
+        return response()->json([
+            'message' => 'Journal created successfully.',
+            'journal' => $journal,
+        ], 200);
+    }
 }
