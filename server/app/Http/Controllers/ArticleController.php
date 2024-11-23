58a80af304491e45394c2a6fbc114979efb2b176
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-23 19:46:33 +0200

    Added get articles by user endpoint

--- server/app/Http/Controllers/ArticleController.php
+++ server/app/Http/Controllers/ArticleController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function getArticles(){
+        $articles = Article::all();
+        return response()->json($articles, 200);
+    }
+
+    public function getArticlesByUser($id){
+        $user = User::find($id);
+
+        if(!$user){
+            return response()->json(["message" => "Not found"], 404);
+        }
+
+
+        $articles = Article::where("posted_by", "=", $user->id);
+
+        return response()->json($articles, 200);
+    }
+
+
+    public function postArticle(Request $request){
         
     }
 }
