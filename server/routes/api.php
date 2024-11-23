c4388ff878cb7d649d11b3e4887b7bb602adf154
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-23 20:55:44 +0200

    Fixed an error in api.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -6,20 +6,20 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix("user")->withoutMiddleware(["csrf"])->group(function(){
+Route::prefix("/user")->withoutMiddleware(["csrf"])->group(function(){
   Route::post("/login", [UserController::class, "login"]);
   Route::post("/register", [UserController::class, "register"]);
 });
 
 
-Route::prefix("journal")->group(function(){
+Route::prefix("/journal")->group(function(){
   Route::get("/{id}", [JournalController::class, "getJournals"]);
   Route::post("/{id}", [JournalController::class, "postJournal"]);
   Route::put("/{id}", [JournalController::class, "editJournal"] );
   Route::delete("/{id}", [JournalController::class, "removeJournal"]);
 });
 
-Route::prefix("article")->group(function(){
+Route::prefix("/article")->group(function(){
   Route::get("/", [ArticleController::class, "getArticles"]);
   Route::get("/{id}", [ArticleController::class, "getArticlesByUser"] );
   Route::post("/{id}", [ArticleController::class, "postArticle"]);
